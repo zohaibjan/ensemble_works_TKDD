32851fed880821e31bde949be876e552e2fc7bba
Muhammad Zohaib Jan <zohaibjan@gmail.com>
2019-08-13 16:06:16 +1000

    Update mainProgram.m

--- mainProgram.m
+++ mainProgram.m
@@ -9,7 +9,7 @@ Problem={'breast-cancer-wisconsin','diabetic_retinopathy','ecoli',...
 % Problem = {'wine'};
 %
 %% Model SETTINGS
-params.numOfRuns = 1;
+params.numOfRuns = 10;
 params.numOfFolds = 10;                   % Create CROSS VALIDATION FOLDS
 params.classifiers = {'ANN', 'KNN', 'DT', 'DISCR','NB','SVM'};
 params.trainFunctionANN={'trainlm','trainbfg','trainrp','trainscg','traincgb','traincgf','traincgp','trainoss','traingdx'};
@@ -17,20 +17,23 @@ params.trainFunctionDiscriminant = {'pseudoLinear','pseudoQuadratic'};
 params.kernelFunctionSVM={'gaussian','polynomial','linear'};
 
 %% MAIN LOOP
-for i = 1:length(Problem)
+parfor i = 1:length(Problem)
+    results = {};
+    nonOptimized_Accuracy = [];
+    optimized_Accuracy = [];
+	p_name = [];
     for runs = 1:params.numOfRuns
         p_name = Problem{i};
         results = runTraining(p_name, params);
-        results.p_name = p_name;
-        saveResults(results);
+        nonOptimized_Accuracy(runs) = results.nonOptimized_Accuracy;
+		optimized_Accuracy(runs) = results.optimized_Accuracy
     end
-    % results.p_name = p_name;
-    % results.nonOptimized_Accuracy = mean(nonOptimized_Accuracy);
-    % results.optimized_Accuracy = mean(optimized_Accuracy);
-    % results.nonOptimized_stdDEV = std(nonOptimized_Accuracy);
-    % results.optimized_stdDEV = std(optimized_Accuracy);
-    % saveResults(results);
+    results.p_name = p_name;
+    results.nonOptimized_Accuracy = mean(nonOptimized_Accuracy);
+    results.optimized_Accuracy = mean(optimized_Accuracy);
+    results.nonOptimized_stdDEV = std(nonOptimized_Accuracy);
+    results.optimized_stdDEV = std(optimized_Accuracy);
+    saveResults(results);
 end
-
 end
 
